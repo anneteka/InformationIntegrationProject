0b6aba8de6dacfc357e06d14bc9e7c0168d64359
sandrix <sandragarciabescos@hotmail.es>
2021-11-10 14:16:13 +0100

    added entity for second source!

--- src/main/java/main/database/entity/EBookSecond.java
+++ src/main/java/main/database/entity/EBookSecond.java
@@ -21,51 +21,52 @@ import lombok.ToString;
 @ToString
 @EqualsAndHashCode
 @Entity
-@Table(name= "books1")
-public class EBookFirst {
+@Table(name= "books2")
+public class EBookSecond {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Long id;
-    @Column(name = "isbn13")
-    private String isbn13;
-    @Column(name = "isbn10")
-    private String isbn10;
+    @Column(name = "book_id")
+    private Long bookID;
     @Column(name = "title")
     private String title;
-    @Column(name = "subtitle")
-    private String subtitle;
     @Column(name = "authors")
     private String authors;
-    @Column(name = "categories")
-    private String categories;
-    @Column(name = "thumbnail")
-    private String thumbnail;
-    @Column(name = "description")
-    private String description;
-    @Column(name = "published_year")
-    private String publishedYear;
     @Column(name = "average_rating")
     private float averageRating;
+    @Column(name = "isbn")
+    private String isbn;
+    @Column(name = "isbn13")
+    private String isbn13;
+    @Column(name = "language_code")
+    private String languageCode;
     @Column(name = "num_pages")
     private Long numPages;
     @Column(name = "rating_count")
     private Long ratingCount;
+    @Column(name = "text_review_count")
+    private Long textReviewsCount;
+    @Column(name = "publication_date")
+    private String publicationDate; 
+    @Column(name = "publisher")
+    private String publisher;
 
-    public EBookFirst(String isbn13, String isbn10, String title, String subtitle, 
-                        String authors, String categories, String thumbnail, String description,
-                            String publishedYear, float averageRating, Long numPages, Long ratingCount){
-        this.isbn13 = isbn13;
-        this.isbn10 = isbn10;
+    public EBookSecond(Long bookID, String title, String authors, float averageRating, String isbn,
+                         String isbn13, String languageCode, Long numPages, Long ratingCount,
+                            Long textReviewsCount, String publicationDate, String publisher){
+        this.bookID = bookID;
         this.title = title;
-        this.subtitle = subtitle;
         this.authors = authors;
-        this.categories = categories;
-        this.thumbnail = thumbnail;
-        this.description = description;
-        this.publishedYear = publishedYear;
         this.averageRating = averageRating;
+        this.isbn = isbn;
+        this.isbn13 = isbn13;
+        this.languageCode = languageCode;
         this.numPages = numPages;
         this.ratingCount = ratingCount;
+        this.textReviewsCount = textReviewsCount;
+        this.publicationDate = publicationDate;
+        this.publisher = publisher;
     }
+
 }
