120737bdd7ab853048ecaac885a4b5bef299a299
anneteka <a.karlysheva@ukma.edu.ua>
2021-11-11 18:30:57 +0100

    added comment

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -12,6 +12,9 @@ public class Application {
     public static void main(String[] args) throws FileNotFoundException {
 
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // use one of the two methods to fill in data to the databases when the app starts running
+        // you only need to run it once and only need one of them
         //context.getBean(BookService.class).setUpFromSources();
         context.getBean(BookService.class).setUpTestSources();
     }
