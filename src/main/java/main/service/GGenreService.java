de5a50239145b9500d0a722d65b994ce4e4703b9
anna <a.karlysheva@ukma.edu.ua>
2021-12-18 16:25:19 +0100

    genre service

--- src/main/java/main/service/GGenreService.java
+++ src/main/java/main/service/GGenreService.java
@@ -2,11 +2,17 @@ package main.service;
 
 import main.database.entity.global.EGlobalGenre;
 import main.database.repository.GlobalGenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GGenreService {
     private GlobalGenreRepository repository;
+
+    @Autowired
+    public GGenreService(GlobalGenreRepository repository) {
+        this.repository = repository;
+    }
 
     public EGlobalGenre saveGenre(String genre){
         return repository.save(new EGlobalGenre(genre));
