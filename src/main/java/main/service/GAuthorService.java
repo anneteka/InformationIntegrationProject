58ee56f48d3c1f53bd1779597939c94fc034fe9a
sandrix <sandragarciabescos@hotmail.es>
2022-02-01 19:01:14 +0100

    second try at author merging

--- src/main/java/main/service/GAuthorService.java
+++ src/main/java/main/service/GAuthorService.java
@@ -37,20 +37,28 @@ public class GAuthorService {
         String[] names1 = keep.getName().split(" ");
         String[] names2 = discard.getName().split(" ");
 
+        // sort arrays from longest name to shortes to first find full name matches
+        Arrays.sort(names1, (str1, str2) -> str2.length() - str1.length());
+        Arrays.sort(names2, (str1, str2) -> str2.length() - str1.length());
+
         int minMatches = Math.min(Math.min(names1.length, names2.length), 2); // at least 2 matching name parts or one if only one name given
 
         int matches = 0;
 
         for (String name1: names1){
             for (String name2: names2){
-                if (name1.equals(name2)) matches++;
+                if (name1.length() > 2 && name2.length() > 2){ // names are full names, not only letter + dot
+                    if (name1.equals(name2)) matches++;
+
+                }else if (matches > 0){                         // only compare name with dot if a "real" match was already found
+                    if (name1.equals(name2)) matches++;
+                }
             }
         }
 
-        if (matches < minMatches){
-            // not enough matches, not the same author
-        }else{
+        if (matches >= minMatches){
             // enough matches, discard second author
+            repository.delete(discard);
         }
     }
 }
